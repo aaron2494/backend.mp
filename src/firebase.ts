fd95583baa1171d2f62b1597d542dea6adec2fd8
aaron Francolino <aaron.e.francolino@gmail.com>
2025-05-27 14:37:22 -0300

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,26 +1,13 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-// Validar variables
-const { FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY } = process.env;
-
-if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
-  throw new Error('❌ Faltan variables de entorno necesarias para Firebase.');
-}
-
-const firebaseConfig = {
-  projectId: FIREBASE_PROJECT_ID,
-  privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  clientEmail: FIREBASE_CLIENT_EMAIL,
-};
-
 const app = initializeApp({
-  credential: cert(firebaseConfig)
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  })
 });
 
 const db = getFirestore(app);
-
 export { db };
